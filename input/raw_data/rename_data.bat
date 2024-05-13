aa7b1347fd8b03d3d3ce7bc808152932da83cfb9
shayantaqi <taqi.shayan@gmail.com>
2024-05-13 22:27:30 +0430

    First commit

---
+++ input/raw_data/rename_data.bat
@@ -0,0 +1,10 @@
+@echo off
+rename "EERA Public School - HeadmasterPrinciple Interview V3.xlsx" "Tool 1.EERA Public School - HeadmasterPrinciple Interview R3.xlsx"
+rename "EERA Public School - Light Tool V3.xlsx" "Tool 2.EERA Public School - Light Tool R3.xlsx"
+rename "EERA Public School - Student Document & Headcount V3.xlsx" "Tool 3.EERA Public School - Student Document & Headcount R3.xlsx"
+rename "EERA Public School - Teacher Tool V3.xlsx" "Tool 4.EERA Public School - Teacher Tool R3.xlsx"
+rename "EERA Public School - WASH Observation V3.xlsx" "Tool 5.EERA Public School - WASH Observation R3.xlsx"
+rename "EERA Public School - Parent Tool V3.xlsx" "Tool 6.EERA Public SchoolCBE - Parent Tool R3.xlsx"
+rename "EERA Public School - Shura Tool V3.xlsx" "Tool 7.EERA Public SchoolCBE - Shura Tool R3.xlsx"
+echo Renaming Done!
+pause
